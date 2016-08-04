0d3ebdd8d7b46c34bbc5cf365dca97dfc24d1c0e
Jordan Crawford <jordan.crawford@me.com>
2016-08-04 21:10:40 +1200

    Almost get a basic list view working.

--- app/src/main/java/kiwi/jordancrawford/whattomake/MainActivity.java
+++ app/src/main/java/kiwi/jordancrawford/whattomake/MainActivity.java
@@ -2,12 +2,32 @@ package kiwi.jordancrawford.whattomake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerViewAdapter;
+    private RecyclerView.LayoutManager recyclerViewLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the recycler view.
+        recyclerView = (RecyclerView) findViewById(R.id.list_recycler_view);
+
+        // Setup a linear layout manager.
+        recyclerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+
+        // Setup our adapter.
+        String[] data = new String[3];
+        data[0] = "Bananas";
+        data[1] = "Cake";
+        data[2] = "Yoghurt";
+        recyclerViewAdapter = new MealListAdapter(data);
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 }
---
+++ app/src/main/java/kiwi/jordancrawford/whattomake/MealListAdapter.java
@@ -0,0 +1,71 @@
+package kiwi.jordancrawford.whattomake;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Jordan on 4/08/16.
+ */
+public class MealListAdapter extends RecyclerView.Adapter<MealListAdapter.MealViewHolder> {
+    String[] strings;
+    public MealListAdapter(String[] strings) {
+        // TODO: Switch to Meals.
+        this.strings = strings;
+    }
+
+    /**
+     * Define a ViewHolder that keeps track of view fields for meals.
+     */
+    public class MealViewHolder extends RecyclerView.ViewHolder {
+        public TextView view;
+        public MealViewHolder(TextView view) {
+            super(view);
+            this.view = view;
+        }
+    }
+
+    /**
+     * When a view holder is created, inflate the layout and setup a ViewHolder object.
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public MealViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Create a view to display the meal.
+        View inflatedView = LayoutInflater.from(parent.getContext()).inflate(R.layout.meal_card, parent, false);
+
+        // Get the text view from insight the inflatedView.
+        TextView mealCardName = (TextView)inflatedView.findViewById(R.id.meal_card_name);
+
+        return new MealViewHolder(mealCardName);
+    }
+
+    /**
+     * When a view holder is bound to the view, setup the view holder with data.
+     *
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(MealViewHolder holder, int position) {
+        // Get the item for this index.
+        String currentItem = strings[position];
+
+        // Setup the view holder with the data.
+        holder.view.setText(currentItem);
+    }
+
+    /**
+     * Return the length of the dataset provided.
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return strings.length;
+    }
+}
