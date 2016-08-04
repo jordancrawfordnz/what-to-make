bee871d7b62464e59fe66a02f4383bdd1ce10d55
Jordan Crawford <jordan.crawford@me.com>
2016-08-04 19:27:49 +1200

    Experiment with the Android example app.

--- app/src/main/java/kiwi/jordancrawford/whattomake/MainActivity.java
+++ app/src/main/java/kiwi/jordancrawford/whattomake/MainActivity.java
@@ -1,13 +1,26 @@
 package kiwi.jordancrawford.whattomake;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "kiwi.jordancrawford.whattomake.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        System.out.println("Send message pressed");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
